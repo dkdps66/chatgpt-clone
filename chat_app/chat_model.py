a6ac79670189504946659b56427bd38c68bc63c3
dkdps66 <dkdps6606@gmail.com>
2024-10-08 12:40:37 +0900

    aimessage

--- chat_app/chat_model.py
+++ chat_app/chat_model.py
@@ -1,5 +1,6 @@
 from langchain_aws.chat_models import ChatBedrock
 from langgraph.graph import StateGraph, MessagesState, START, END
+from langchain_core.messages import AIMessage, AIMessageChunk
 
 class ChatModel:
     """Chat 모델 클래스는 주어진 모델 ID로 대화 모델을 초기화하고 요청에 대한 응답을 제공합니다."""
@@ -12,6 +13,7 @@ class ChatModel:
         self.graph_builder.add_edge(START, 'model')
         self.graph_builder.add_edge('model', END)
         self.graph = self.graph_builder.compile()
+        self.config = {'configurable': {'thread_id': '1'}}
 
 
     def _call_model(self, state: MessagesState):
@@ -19,4 +21,10 @@ class ChatModel:
     
     def get_response(self, prompt: str):
         """모델에 대한 요청을 보내고 응답을 받는다."""
-        return self.chat_model.stream(prompt)
+        for chunk, metadata in self.graph.stream(
+            {'messages': prompt},
+            config=self.config,
+            stream_mode='messages'
+        ):
+            if isinstance(chunk, AIMessage):
+                yield chunk.content
